4650b9497098c30bfb979c3b33fe1d78c71cb893
Peter Kofler <peter.kofler@code-cop.org>
2018-03-25 14:20:24 +0200

    Fromat Groovy script.

--- src/main/walkmod/scripts/fields.groovy
+++ src/main/walkmod/scripts/fields.groovy
@@ -1,14 +1,15 @@
 // make all fields private
-import org.walkmod.javalang.ast.body.ModifierSet;
-import java.lang.reflect.Modifier;
-import org.walkmod.javalang.ast.body.FieldDeclaration;
+import java.lang.reflect.Modifier
 
-for(type in node.types){    
-  def fields = type.members.findAll({it instanceof FieldDeclaration});
-  for (field in fields){
-    modifiers = ModifierSet.addModifier(field.modifiers, Modifier.PRIVATE);
-    modifiers = ModifierSet.removeModifier(modifiers, Modifier.PROTECTED);
-    modifiers = ModifierSet.removeModifier(modifiers, Modifier.PUBLIC);
-    field.setModifiers(modifiers);
-  }
+import org.walkmod.javalang.ast.body.FieldDeclaration
+import org.walkmod.javalang.ast.body.ModifierSet
+
+for(type in node.types){
+    def fields = type.members.findAll({it instanceof FieldDeclaration})
+    for (field in fields){
+        modifiers = ModifierSet.addModifier(field.modifiers, Modifier.PRIVATE)
+        modifiers = ModifierSet.removeModifier(modifiers, Modifier.PROTECTED)
+        modifiers = ModifierSet.removeModifier(modifiers, Modifier.PUBLIC)
+        field.setModifiers(modifiers)
+    }
 }
