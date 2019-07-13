76efa30b125f0e233b401fbd4142d5cd90bca946
Peter Kofler <peter.kofler@code-cop.org>
2019-07-13 23:18:21 +0200

    Create default constructor.

--- src/main/walkmod/change_injection/inject.groovy
+++ src/main/walkmod/change_injection/inject.groovy
@@ -1,7 +1,11 @@
 // change all injectons to constructor
+import java.lang.reflect.Modifier
 import org.walkmod.javalang.ast.CompilationUnit
-import org.walkmod.javalang.ast.body.TypeDeclaration
+import org.walkmod.javalang.ast.body.ConstructorDeclaration
 import org.walkmod.javalang.ast.body.FieldDeclaration
+import org.walkmod.javalang.ast.body.ModifierSet
+import org.walkmod.javalang.ast.body.TypeDeclaration
+import org.walkmod.javalang.ast.stmt.BlockStmt
 
 import groovy.inspect.Inspector.MemberComparator
 
@@ -17,20 +21,41 @@ def processType(TypeDeclaration type) {
     if (!fields) {
         return
     }
-    
+
     println("--- class " + type.name)
-    println(fields)
+    // println(fields)
     // fields.each { removeAnnotation(it) }
+
+    constructors = findConstuctors(type)
+    println(constructors)
+    if (!constructors) {
+        modifiers = ModifierSet.PUBLIC
+        constructor = new ConstructorDeclaration(modifiers, type.name)
+        constructor.block = new BlockStmt();
+        
+        constructors.add(constructor)
+        type.members.add(0, constructor)
+    }
+
+    // if none create one
+    // add @Inject to constructor (if not there)
+    // add argument to list
+    // add setting to body
 }
 
 def findInjectedFields(TypeDeclaration type) {
     return type.members.
-            findAll { it instanceof org.walkmod.javalang.ast.body.FieldDeclaration }.
+            findAll { it instanceof FieldDeclaration }.
             findAll { it.annotations.any { it.name.name == "Inject" } }
 }
 
 def removeAnnotation(FieldDeclaration field) {
     field.annotations = field.annotations.findAll { it.name.name != "Inject" }
+}
+
+def findConstuctors(TypeDeclaration type) {
+    return type.members.
+            findAll { it instanceof ConstructorDeclaration }
 }
 
 // available bindings:
@@ -39,3 +64,10 @@ def removeAnnotation(FieldDeclaration field) {
 // org.walkmod.query.QueryEngine query;
 
 processFile(node)
+
+/*
+ * TODO bot handled cases:
+ * Multiple fields in one variable declaration.
+ * Multiple constructors to be modified.
+ * Generated constructor not after all fields.
+ */
