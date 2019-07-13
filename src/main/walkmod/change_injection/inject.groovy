fcc3595678c247474b8dbbe93a4db0ae48a01a1f
Peter Kofler <peter.kofler@code-cop.org>
2019-07-13 23:03:39 +0200

    Remove @Inject annotation.

--- src/main/walkmod/change_injection/inject.groovy
+++ src/main/walkmod/change_injection/inject.groovy
@@ -1,41 +1,41 @@
 // change all injectons to constructor
 import org.walkmod.javalang.ast.CompilationUnit
 import org.walkmod.javalang.ast.body.TypeDeclaration
-import org.walkmod.walkers.VisitorContext
+import org.walkmod.javalang.ast.body.FieldDeclaration
 
 import groovy.inspect.Inspector.MemberComparator
 
-import org.walkmod.query.QueryEngine
+
+def processFile(CompilationUnit file) {
+    // println(file.fileName)
+    // println(file.getPackage())
+    file.types.each { processType(it) }
+}
+
+def processType(TypeDeclaration type) {
+    fields = findInjectedFields(type)
+    if (!fields) {
+        return
+    }
+    
+    println("--- class " + type.name)
+    println(fields)
+    // fields.each { removeAnnotation(it) }
+}
+
+def findInjectedFields(TypeDeclaration type) {
+    return type.members.
+            findAll { it instanceof org.walkmod.javalang.ast.body.FieldDeclaration }.
+            findAll { it.annotations.any { it.name.name == "Inject" } }
+}
+
+def removeAnnotation(FieldDeclaration field) {
+    field.annotations = field.annotations.findAll { it.name.name != "Inject" }
+}
 
 // available bindings:
 // org.walkmod.javalang.ast.CompilationUnit node;
 // org.walkmod.walkers.VisitorContext context;
 // org.walkmod.query.QueryEngine query;
 
-def findInjectedFields(TypeDeclaration type) {
-    return type.members.
-        findAll { it instanceof org.walkmod.javalang.ast.body.FieldDeclaration }.
-        findAll { it.annotations.any { it.name.name == "Inject" } }
-}
-
-def cu(CompilationUnit node) {
-    // println(node.fileName)
-    // println(node.getPackage())
-    for(type in node.types) {
-        println("--- class " + type.name)
-        println(findInjectedFields(type))
-//  println(member.type);
-//  println(member.variables);
-//  member.annotations = [] // drop annotation
-
-        println("")
-    }
-}
-
-def vc(VisitorContext context) {
-    println(context.entrySet())
-}
-
-println("Hello from Script")
-cu(node)
-// vc(context)
+processFile(node)
